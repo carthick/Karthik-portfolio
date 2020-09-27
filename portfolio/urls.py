d30c1e9f6a7557a9e7ed3cee8f65943e220febe7
carthick <challakarthik@gmail.com>
2020-09-27 12:12:13 -0400

    url route

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', include('projects.urls')),
+    path('', include('projects.urls')),
 ]
 
 if settings.DEBUG:
